f6900b5a7294b2a1870c244adbfe9298e782ff5d
Stephanie Xue <stephaniexue3@gmail.com>
2024-11-10 10:25:27 -0800

    create new project

---
+++ src/client/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hack-project</h1>    
+      <h2>Hi</h2>
+    </div>
+  );
+}
+
+export default App;
