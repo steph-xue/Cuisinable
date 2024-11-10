fdae0adb0e264b3ef26157505086651cdcf152ea
Stephanie Xue <stephaniexue3@gmail.com>
2024-11-10 10:54:58 -0800

    fix landing page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Header from "./components/Header";
 import Landing from "./components/Landing";
-import Footer from "./components/Footer";
 
 function App() {
 
@@ -15,9 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
       <Landing />
-      <Footer />
     </div>
   );
 }
--- src/client/components/Landing.jsx
+++ src/client/components/Landing.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Landing() {
   return (
     <div className="landing-page">
-      <h1 className="landing-title">Project title</h1>
+      <h1 className="landing-title">Cuisinably</h1>
+      <button className="start-button">Get Started</button>
       <img className="bear" src="/images/bear-chef.png" alt="bear chef" />
-      <p className="description">description</p>
-      <button className="start-button">Start</button>
+      <p className="description">Generate recipes based on food allergies and intolerances</p>
     </div>
   );
 }
