52759124b7e5415e35a98510b4824cb5262ba610
yu38 <yu38@students.cs.ubc.ca>
2024-11-10 11:48:01 -0800

    Implemented spoonacular API fetching

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,35 @@
 import express from "express";
-import ViteExpress from "vite-express";
+import cors from "cors";
 
+const apiKey = "cbdcac716ecf4c738d0bf2f708d80e0c"
+
+// const express = require("express");
+// const cors = require("cors");
+const port = 8000;
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
+// Use CORS to allow requests from the frontend
+app.use(cors());
+
+// Use express.json() to parse incoming JSON requests
+app.use(express.json());
+
+// GET route to send the list of tasks to the frontend
+app.get("/selections", (req, res) => {
+    const cuisine = req.query.cuisine
+    const intolerances = req.query.intolerances
+
+    const endpoint = `cuisine=${cuisine}&intolerances=${intolerances}&number=10&addRecipeInformation=true&addRecipeInstructions=true&instructionsRequired=true`
+
+    const apiUrl = "https://api.spoonacular.com/recipes/complexSearch?" + endpoint + "&apiKey=" + apiKey
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(jsonData => { console.log(jsonData); return res.json(jsonData) })
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
-);
+// Start the server and listen for requests on port 3000
+app.listen( port, () => {
+    console.log(`Server running on http://localhost:${port}
+`);
+});
