5763fb69e60a87556ec50e9a0d11c9919483119d
Stephanie Xue <stephaniexue3@gmail.com>
2024-11-10 12:54:55 -0800

    fix selector page radio buttons

--- src/client/components/Selector.jsx
+++ src/client/components/Selector.jsx
@@ -57,88 +57,89 @@ function Selector() {
                 </form>
                 <h2 className="selector-cuisines">Select your preferred cuisine</h2>
                 <form className="selector-cuisines-form">
-                    <div class="radio-box">
+                    <div class="radio-box selector-container">
                         <div>
                             <input type="radio" className="btn-check" id="american" name="cuisine-answer" value="american" />
-                            <label className="btn btn-secondary" htmlFor="american">American</label>
+                            <label className="btn btn-select-cuisine" htmlFor="american">American</label>
                         </div>
                         <div>
-                            <input type="radio" id="caribbean" name="cuisine-answer" value="caribbean" />
-                            <label htmlFor="caribbean">Caribbean</label>
+                            <input type="radio" className="btn-check" id="caribbean" name="cuisine-answer" value="caribbean" />
+                            <label className="btn btn-select-cuisine" htmlFor="caribbean">Caribbean</label>
                         </div>
                         <div>
-                            <input type="radio" id="chinese" name="cuisine-answer" value="chinese" />
-                            <label htmlFor="chinese">Chinese</label>
+                            <input type="radio" className="btn-check" id="chinese" name="cuisine-answer" value="chinese" />
+                            <label className="btn btn-select-cuisine" htmlFor="chinese">Chinese</label>
                         </div>
                         <div>
-                            <input type="radio" id="eastern-european" name="cuisine-answer" value="eastern-european" />
-                            <label htmlFor="eastern-european">Eastern European</label>
+                            <input type="radio" className="btn-check" id="eastern-european" name="cuisine-answer" value="eastern-european" />
+                            <label className="btn btn-select-cuisine" htmlFor="eastern-european">Eastern European</label>
                         </div>
                         <div>
-                            <input type="radio" id="french" name="cuisine-answer" value="french" />
-                            <label htmlFor="french">French</label>
+                            <input type="radio" className="btn-check" id="french" name="cuisine-answer" value="french" />
+                            <label className="btn btn-select-cuisine" htmlFor="french">French</label>
                         </div>
                         <div>
-                            <input type="radio" id="german" name="cuisine-answer" value="german" />
-                            <label htmlFor="german">German</label>
+                            <input type="radio" className="btn-check" id="german" name="cuisine-answer" value="german" />
+                            <label className="btn btn-select-cuisine" htmlFor="german">German</label>
                         </div>
                         <div>
-                            <input type="radio" id="greek" name="cuisine-answer" value="greek" />
-                            <label htmlFor="greek">Greek</label>
+                            <input type="radio" className="btn-check" id="greek" name="cuisine-answer" value="greek" />
+                            <label className="btn btn-select-cuisine" htmlFor="greek">Greek</label>
                         </div>
                         <div>
-                            <input type="radio" id="indian" name="cuisine-answer" value="indian" />
-                            <label htmlFor="indian">Indian</label>
+                            <input type="radio" className="btn-check" id="indian" name="cuisine-answer" value="indian" />
+                            <label className="btn btn-select-cuisine" htmlFor="indian">Indian</label>
                         </div>
                         <div>
-                            <input type="radio" id="italian" name="cuisine-answer" value="italian" />
-                            <label htmlFor="italian">Italian</label>
+                            <input type="radio" className="btn-check" id="italian" name="cuisine-answer" value="italian" />
+                            <label className="btn btn-select-cuisine" htmlFor="italian">Italian</label>
                         </div>
                         <div>
-                            <input type="radio" id="japanese" name="cuisine-answer" value="japanese" />
-                            <label htmlFor="japanese">Japanese</label>
+                            <input type="radio" className="btn-check" id="japanese" name="cuisine-answer" value="japanese" />
+                            <label className="btn btn-select-cuisine" htmlFor="japanese">Japanese</label>
                         </div>
                         <div>
-                            <input type="radio" id="jewish" name="cuisine-answer" value="jewish" />
-                            <label htmlFor="jewish">Jewish</label>
+                            <input type="radio" className="btn-check" id="jewish" name="cuisine-answer" value="jewish" />
+                            <label className="btn btn-select-cuisine" htmlFor="jewish">Jewish</label>
                         </div>
                         <div>
-                            <input type="radio" id="korean" name="cuisine-answer" value="korean" />
-                            <label htmlFor="korean">Korean</label>
+                            <input type="radio" className="btn-check" id="korean" name="cuisine-answer" value="korean" />
+                            <label className="btn btn-select-cuisine" htmlFor="korean">Korean</label>
                         </div>
                         <div>
-                            <input type="radio" id="latin-american" name="cuisine-answer" value="latin-american" />
-                            <label htmlFor="latin-american">Latin American</label>
+                            <input type="radio" className="btn-check" id="latin-american" name="cuisine-answer" value="latin-american" />
+                            <label className="btn btn-select-cuisine" htmlFor="latin-american">Latin American</label>
                         </div>
                         <div>
-                            <input type="radio" id="mediterranean" name="cuisine-answer" value="mediterranean" />
-                            <label htmlFor="mediterranean">Mediterranean</label>
+                            <input type="radio" className="btn-check" id="mediterranean" name="cuisine-answer" value="mediterranean" />
+                            <label className="btn btn-select-cuisine" htmlFor="mediterranean">Mediterranean</label>
                         </div>
                         <div>
-                            <input type="radio" id="mexican" name="cuisine-answer" value="mexican" />
-                            <label htmlFor="mexican">Mexican</label>
+                            <input type="radio" className="btn-check" id="mexican" name="cuisine-answer" value="mexican" />
+                            <label className="btn btn-select-cuisine" htmlFor="mexican">Mexican</label>
                         </div>
                         <div>
-                            <input type="radio" id="middle-eastern" name="cuisine-answer" value="middle-eastern" />
-                            <label htmlFor="middle-eastern">Middle Eastern</label>
+                            <input type="radio" className="btn-check" id="middle-eastern" name="cuisine-answer" value="middle-eastern" />
+                            <label className="btn btn-select-cuisine" htmlFor="middle-eastern">Middle Eastern</label>
                         </div>
                         <div>
-                            <input type="radio" id="spanish" name="cuisine-answer" value="spanish" />
-                            <label htmlFor="african">Spanish</label>
+                            <input type="radio" className="btn-check" id="spanish" name="cuisine-answer" value="spanish" />
+                            <label className="btn btn-select-cuisine" htmlFor="african">Spanish</label>
                         </div>
                         <div>
-                            <input type="radio" id="thai" name="cuisine-answer" value="thai" />
-                            <label htmlFor="thai">Thai</label>
+                            <input type="radio" className="btn-check" id="thai" name="cuisine-answer" value="thai" />
+                            <label className="btn btn-select-cuisine" htmlFor="thai">Thai</label>
                         </div>
                         <div>
-                            <input type="radio" id="vietnamese" name="cuisine-answer" value="vietnamese" />
-                            <label htmlFor="vietnamese">Vietnamese</label>
+                            <input type="radio" className="btn-check" id="vietnamese" name="cuisine-answer" value="vietnamese" />
+                            <label className="btn btn-select-cuisine" htmlFor="vietnamese">Vietnamese</label>
                         </div>
                     </div>
                 </form>
             <button className="button">Get Recipes</button>
         </div>
         <div className="selector-bear">
+            <h1 className="selector-bear-title">Cuisinably</h1>
             <img className="selector-bear-image" src="/images/bear-chef.png" alt="bear chef" />
         </div>
     
